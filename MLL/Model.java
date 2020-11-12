242a7763ef2232767ffd6afb45b00e75c1746574
caraingram <73135583+caraingram@users.noreply.github.com>
2020-11-11 17:51:04 -0800

    Update Model.java

--- MLL/Model.java
+++ MLL/Model.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 
 public abstract class Model {
 
-  public abstract void train(ArrayList<DataPoint> sampleData);
+  public abstract void train(ArrayList<DataPoint> data);
   public abstract String test(ArrayList<DataPoint> data);
-  public abstract Double getAccuracy(ArrayList<DataPoint> data);
-  public abstract Double getPrecision(ArrayList<DataPoint> data);
+  public abstract double getAccuracy(ArrayList<DataPoint> data);
+  public abstract double getPrecision(ArrayList<DataPoint> data);
 
 }
